699c83d2b6789e60d55872002b2083f85e1341dc
liveseyy <dima74ermilov@gmail.com>
2021-02-02 17:01:04 +0500

    Улучшил визуальную часть при помощи Bootstrap4

--- educa/urls.py
+++ educa/urls.py
@@ -20,10 +20,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('courses.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
-    path('course/', include('courses.urls')),
     path('students/', include('students.urls')),
     path('api/', include('courses.api.urls', namespace='api')),
 ]
--- students/urls.py
+++ students/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
          views.StudentCourseListView.as_view(),
          name='student_course_list'),
     path('courses/<pk>/',
-         cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
+         cache_page(1)(views.StudentCourseDetailView.as_view()),
          name='student_course_detail'),
     path('courses/<pk>/<module_id>/',
-         cache_page(60 * 15)(views.StudentCourseDetailView.as_view()),
+         cache_page(1)(views.StudentCourseDetailView.as_view()),
          name='student_course_detail_module'),
 ]
